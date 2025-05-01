fedfe2c4abd8728f21bc288bd69d0bcdca8879af
Siro Daves <sirodevske@gmail.com>
2025-05-01 08:33:45 +0300

    core data manager

---
+++ SongLib/Presentation/ViewModels/SelectionService.swift
@@ -0,0 +1,23 @@
+//
+//  SelectionService.swift
+//  SongLib
+//
+//  Created by Siro Daves on 30/04/2025.
+//
+
+
+protocol SelectionService {
+    var books: [Book] { get }
+    var addedBook: Book? { get }
+    func addBook(_ book: Book)
+}
+
+final class SelectionManager: SelectionService {
+    private(set) var books: [Book] = []
+    private(set) var addedBook: Book? = nil
+    
+    func addBook(_ book: Book) {
+        books.append(book)
+        addedBook = book
+    }
+}
