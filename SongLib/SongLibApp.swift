24a8c43ded5c3b0df0a3be9fff31b2b11298bd59
Siro Daves <sirodevske@gmail.com>
2025-05-03 22:29:53 +0300

    refining the selection ui

--- SongLib/SongLibApp.swift
+++ SongLib/SongLibApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SongLibApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(prefsRepo: PrefsRepository())
         }
     }
 }
